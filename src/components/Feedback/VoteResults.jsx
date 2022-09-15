a820635e5c532be723cbd87cc79aaad83238dfc7
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-09-15 20:36:22 +0300

    message no any feedback

--- src/components/Feedback/VoteResults.jsx
+++ src/components/Feedback/VoteResults.jsx
@@ -1,6 +1,10 @@
 
-export default function VoteResults({good, neutral,bad, total, goodpersentage}) {
+export default function VoteResults({ good, neutral, bad, total, goodpersentage }) {
+    if (good === 0 && neutral === 0 && bad === 0) {
+        return (<p>"There is no feedback"</p>);
+    } 
     return (
+        
         <ul>
             <li>
                 <p>Good: <span>{good}</span> </p>
